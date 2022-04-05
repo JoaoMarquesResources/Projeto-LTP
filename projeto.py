fba8600e81b786f2ac075964f190d8a93c054b22
João Marques <85469488+JoaoMarques-Dev@users.noreply.github.com>
2022-04-05 11:48:57 +0100

    Opcao 1 e tratamento de erros

--- projeto.py
+++ projeto.py
@@ -1,6 +1,43 @@
-print("----- MENU -----")
-print("1 - Gerir Equipas")
-print("2 - Gerir Jogos e Classificações")
-print("3 - Sair")
-print("----------------")
+import random
 
+ListaEquipas = []
+
+while True:
+    print("\n----- MENU -----")
+    print("1 - Gerir Equipas")
+    print("2 - Gerir Jogos e Classificações")
+    print("3 - Sair")
+    print("----------------")
+
+    opcao = int(input("Opção: "))
+
+    if opcao >= 1 and opcao <= 3:
+        if opcao == 1:
+            while True:
+                equipas = int(input("\nNúmero de Equipas: "))
+
+                if equipas < 3:
+                    print("ERRO: Numero de equipas inválido")
+                else: break
+            
+            for i in range(0, equipas):
+                nome = str(input("\nNome da Equipa: "))
+
+                while True:
+                    numJogadores = int(input("Número de jogadores da equipa: "))
+
+                    if numJogadores < 5:
+                        print("ERRO: Número de Jogadores inválido!")
+                    else:
+                        ListaEquipas.append(nome)
+                        ListaEquipas.append(numJogadores)
+                        break
+            print(ListaEquipas)
+
+        elif opcao == 2:
+            for i in range(0, equipas):
+                #gerar random os resultados
+
+        else: break
+    else:
+        print("\nERRO: Opção não existe!")
